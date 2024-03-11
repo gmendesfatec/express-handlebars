c1760b233ad2316631660fe83488b021e74270e7
Gabriel mendes <gabriel.mendes19@fatec.sp.gov.br>
2024-03-11 20:55:56 -0300

    update

--- index.ts
+++ index.ts
@@ -1,7 +1,20 @@
 import express from 'express'
 import { engine } from 'express-handlebars'
 
-const app = express()
+// Express app
+const app = express();
 
-app.engine("handlebars", engine({defaultLayout: "main"}))
-app.set("view engine", "handlebars")
+// Define arquivo .handlebars que será renderizado primeir
+// Então, nesse caso, será ./views/layouts/main.handlebars
+app.engine("handlebars", engine({defaultLayout: "main"}));
+
+// Definindo o handlebars como view engine
+app.set("view engine", "handlebars");
+
+app.get('/', (req, res) => {
+    res.render("primeira_pagina");
+})
+
+app.listen(3000, () => {
+    console.log("Rodando na porta 3000")
+})
